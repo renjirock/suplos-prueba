e193ed4ecf58d67912ea36db90cd6dccf6cd19da
renjirock <mauriciokode@gmail.com>
2021-02-20 01:50:13 -0500

    Se actualiza el buscador en buscador.js

--- src/Resources/js/buscador.js
+++ src/Resources/js/buscador.js
@@ -1,46 +1,7 @@
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // agrega la clase show
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) showClass(x[i], "show");
-  }
-}
 
-// filtro de elementos
-function showClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-// ocualta los elementos que no son seleccionados
-function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+function search(){//buscara los bienes guardados y los imprimira en pantalla
+  let ciudad = document.getElementById('selectCiudad').value;
+  let tipo = document.getElementById('selectTipo').value;
+  let precio = document.getElementById('rangoPrecio').value;
+  location.href="SearchData.php?ciudad="+ciudad+"&tipo="+tipo+"&precio="+precio;
 }
--- src/views/SearchView.php
+++ src/views/SearchView.php
@@ -233,12 +233,6 @@ class SearchView
                             var tipo = document.getElementById("selectTipoExcel").value;
                             location.href="Excel.php?ciudad="+ciudad+"&tipo="+tipo;
                         }
-                        function search(){//buscara los bienes guardados y los imprimira en pantalla
-                            let ciudad = document.getElementById('selectCiudad').value;
-                            let tipo = document.getElementById('selectTipo').value;
-                            let precio = document.getElementById('rangoPrecio').value;
-                            location.href="SearchData.php?ciudad="+ciudad+"&tipo="+tipo+"&precio="+precio;
-                        }
                     </script>
                 </body>
             </html>
